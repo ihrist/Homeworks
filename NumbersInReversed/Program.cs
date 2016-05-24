125fc4235c3f291ff76d8963c6faac4954203c0f
Ivan Hristov <ivan.hristov01@abv.bg>
2016-05-24 03:36:11 +0300

    NumbersInReverse
    Project C#

---
+++ NumbersInReversed/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersInReversed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReversedMethod();
+        }
+        static void ReversedMethod()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int left = n;
+            int rev = 0;
+            while (left > 0)
+            {
+                var r = left % 10;
+                rev = rev * 10 + r;
+                left = left / 10;  //left = Math.floor(left / 10); 
+            }
+
+            Console.WriteLine(rev);
+        }
+    }
+}
