995df2832403524400e926c4a267c7fd1045a6d6
Ivan Hristov <ivan.hristov01@abv.bg>
2016-05-26 10:49:47 +0300

    Game of Names
    A problem from SoftUni Exam that i work on.

---
+++ GameOfNames/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = int.Parse(Console.ReadLine());
+            int[] fourthArray = new int[p];
+            int TotalSum = 0;
+            string PlayerName = string.Empty;
+            long MaxPoints = long.MinValue;
+            string winner = string.Empty;
+            
+
+            for (int i = 0; i < p; i++)
+            {
+                PlayerName = Console.ReadLine();
+                
+                int sum = 0;
+                for (int j = 0; j < PlayerName.Length; j++)
+                {
+                    if (PlayerName[j] % 2 ==0)
+                    {
+                        sum += PlayerName[j];
+                    }
+                    else
+                    {
+                        sum -= PlayerName[j];
+                    }
+                }
+
+                if (sum >= MaxPoints)
+                {
+                    Console.WriteLine("The winner is {0}- {1} points", PlayerName, sum);
+                }
+                
+            }
+
+
+        }
+    }
+}
