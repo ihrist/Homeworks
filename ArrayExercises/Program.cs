9f2c70d6a1fcc0825f432cbc4e00dec99fcc5405
Ivan Hristov <ivan.hristov01@abv.bg>
2016-06-09 21:00:12 +0300

    Exercises

---
+++ ArrayExercises/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RotateAndSum
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+            //deklarirame masiva
+            int[] sumOfRotations = new int[numbers.Length];
+            //deklarirame 4isloto
+            for (int currentrotations = 0; currentrotations < rotations; currentrotations++)
+            {
+                int LastElement = numbers[numbers.Length - 1];
+                for (int currentElement = numbers.Length-1; currentElement > 0; currentElement--)
+                {
+                    numbers[currentElement] = numbers[currentElement - 1];
+                }
+                numbers[0] = LastElement;
+                for (int currentElement = 0; currentElement < numbers.Length; currentElement++)
+                {
+                    sumOfRotations[currentElement] += numbers[currentElement];
+                }
+                
+            }
+            Console.WriteLine(string.Join(" ", sumOfRotations));
+           
+
+        }
+    }
+}
---
+++ FoldAndSumvotoroReshenie/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoldAndSumvotoroReshenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] firstArray = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            char[] secArray = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+            int smallerArrrayLenght = Math.Min(firstArray.Length, secArray.Length);
+            bool areEqual = false;
+
+            for (int i = 0; i < smallerArrrayLenght; i++)
+            {
+                if (firstArray[i] == secArray[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    if (firstArray[i] <= secArray[i])
+                    {
+                        Console.WriteLine(firstArray);
+                        Console.WriteLine(secArray);
+                    }
+                    else
+                    {
+                        Console.WriteLine(secArray);
+                        Console.WriteLine(firstArray);
+                    }
+                    areEqual = true;
+                    break;
+                }
+            }
+            if (!areEqual)
+            {
+                if (firstArray.Length<=secArray.Length)
+                {
+                    Console.WriteLine(firstArray);
+                    Console.WriteLine(secArray);
+                }
+                else
+                {
+                    Console.WriteLine(secArray);
+                    Console.WriteLine(firstArray);
+                }
+            }
+
+
+        }
+    }
+}
